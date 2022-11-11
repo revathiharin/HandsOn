16069fcf388affef90da0ce3fdf999ee03b93f69
Revathi Harin <revathiharin@gmail.com>
2022-11-11 14:43:23 +0100

    Hacker Rank 1 week practise

---
+++ C++/MinMaxSum.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+int main()
+{
+    vector<int> arr{1,3,5,7,9};
+    int minValue = 0, maxValue = arr[0];
+    long int sumValue = 0;
+    if (arr[0] == 0)
+    {
+        minValue = arr[1];
+    }
+    else
+    {
+        minValue = arr[0];
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        sumValue = sumValue + arr[i];
+        if (minValue > arr[i])
+        {
+            minValue = arr[i];
+        }
+        if (maxValue < arr[i])
+        {
+            maxValue = arr[i];
+        }
+    }
+    cout << sumValue - maxValue << " " << sumValue - minValue;
+    return 0;
+}
+
+/*
+void miniMaxSum(vector<int> arr) {
+    int minValue = 0, maxValue = arr[0];
+    long int sumValue = 0;
+    if(arr[0] == 0){minValue = arr[1];}else {minValue = arr[0];}
+    for(int i = 0; i < arr.size(); i++){
+        sumValue = sumValue + arr[i];
+        if(minValue > arr[i]){
+            minValue = arr[i];
+        }
+        if(maxValue < arr[i]){
+             maxValue = arr[i];
+        }
+
+    }
+    cout << sumValue-maxValue << " " << sumValue-minValue;
+}
+
+int main()
+{
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+*/
