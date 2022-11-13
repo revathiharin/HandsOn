e67ebea9caba6dcedf99e0c21228a4d44e04bac9
Revathi Harin <revathiharin@gmail.com>
2022-11-13 19:52:55 +0100

    Restructure

---
+++ C++/LanguageC++/InputandOutput.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int s;
+    int n;
+    int sum;
+    while(cin>>s)
+    {
+        sum = sum + s;
+    }
+   
+    cout <<sum<<endl;
+    return 0;
+}
